d41bbf714968f7ad00e5af1ef380973d58ffe5f8
Pranav Payghan <pranavpayghan@gmail.com>
2026-03-17 22:42:54 +0530

    fix: add ProxyFix to capture real user IPs on Render

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app.routes.auth_routes import auth_bp
 from app.routes.dashboard_routes import dashboard_bp
@@ -8,6 +9,8 @@ from app.routes.dashboard_routes import dashboard_bp
 def create_app():
 
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
+
     CORS(app, origins=[
     "http://localhost:5173",
     "https://ip-sentinel-1.onrender.com"
@@ -19,8 +22,7 @@ def create_app():
     # REGISTER BLUEPRINTS (🔥 IMPORTANT)
     app.register_blueprint(auth_bp)
     app.register_blueprint(dashboard_bp)
-    
-    
+
     @app.route("/ping")
     def ping():
         return {"status": "alive"}, 200
@@ -28,7 +30,5 @@ def create_app():
     @app.route("/")
     def home():
         return {"message": "Backend Running Successfully"}
-    
-    
 
     return app
